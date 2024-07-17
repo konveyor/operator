1f02b6d8b1be1de930d1fc8304fcf83fb46d8e1a
David Zager <dzager@redhat.com>
2024-07-17 15:10:05 -0400

    :seedling: make hack scripts runnable via curl (#356)
    Signed-off-by: David Zager <dzager@redhat.com>

--- hack/start-minikube.sh
+++ hack/start-minikube.sh
@@ -2,20 +2,6 @@
 set -e
 set -x
 
-# Figure out where we are being run from.
-# This relies on script being run from:
-#  - ${PROJECT_ROOT}/hack/install-tackle.sh
-#  - ${PROJECT_ROOT}/bin/install-tackle.sh
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)"
-__repo="$(basename "${__root}")"
-__bin_dir="${__root}/bin"
-__os="$(uname -s | tr '[:upper:]' '[:lower:]')"
-__arch="$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')"
-
-# Update PATH for execution of this script
-export PATH="${__bin_dir}:${PATH}"
-
 # Inputs via environment variables
 MINIKUBE_DRIVER="${MINIKUBE_DRIVER:-}"
 MINIKUBE_CONTAINER_RUNTIME="${MINIKUBE_CONTAINER_RUNTIME:-}"
@@ -23,7 +9,6 @@ MINIKUBE_KUBERNETES_VERSION="${MINIKUBE_KUBERNETES_VERSION:-}"
 MINIKUBE_CPUS="${MINIKUBE_CPUS:-}"
 MINIKUBE_MEMORY="${MINIKUBE_MEMORY:-}"
 MINIKUBE_CNI="${MINIKUBE_CNI:-}"
-OLM="${OLM:-true}"
 
 # Check pre-reqs
 # May want to leave this for the user to install
@@ -53,7 +38,3 @@ fi
 
 # Enable ingress
 minikube addons enable ingress
-
-if [ "${OLM}" = "true" ]; then
-  curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.26.0/install.sh | bash -s v0.26.0
-fi
