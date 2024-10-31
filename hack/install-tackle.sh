377f3e31dbe1026ddb9a0c86ff37e8b292833d5c
Maayan Hadasi <60384172+mguetta1@users.noreply.github.com>
2024-10-31 14:07:07 +0200

    :bug: Set OLM version to 0.28.0 for release branches (#390)
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- hack/install-tackle.sh
+++ hack/install-tackle.sh
@@ -29,6 +29,7 @@ ANALYZER_CONTAINER_REQUESTS_MEMORY="${ANALYZER_CONTAINER_REQUESTS_MEMORY:-0}"
 ANALYZER_CONTAINER_REQUESTS_CPU="${ANALYZER_CONTAINER_REQUESTS_CPU:-0}"
 FEATURE_AUTH_REQUIRED="${FEATURE_AUTH_REQUIRED:-false}"
 TIMEOUT="${TIMEOUT:-15m}"
+OLM_VERSION="${OLM_VERSION:-0.28.0}"
 
 if ! command -v kubectl >/dev/null 2>&1; then
   kubectl_bin="${__bin_dir}/kubectl"
@@ -96,7 +97,7 @@ install_operator() {
   || kubectl get subscription --namespace ${NAMESPACE} -o yaml konveyor-operator # Print subscription details when timed out
 }
 
-kubectl get customresourcedefinitions.apiextensions.k8s.io clusterserviceversions.operators.coreos.com || operator-sdk olm install
+kubectl get customresourcedefinitions.apiextensions.k8s.io clusterserviceversions.operators.coreos.com || operator-sdk olm install --version ${OLM_VERSION}
 olm_namespace=$(kubectl get clusterserviceversions.operators.coreos.com --all-namespaces | grep packageserver | awk '{print $1}')
 kubectl rollout status -w deployment/olm-operator --namespace="${olm_namespace}"
 kubectl rollout status -w deployment/catalog-operator --namespace="${olm_namespace}"
